1df36c4a6ede825c476c66eef2dd0cc3f8fc36bf
SandorEmanuel <sandor_emi@yahoo.com>
2019-01-07 22:15:29 +0200

    set-up simple math operations

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+
+public class App 
+{
+    public static void main( String[] args ){
+        Numbers.addition(5, 7);
+        Numbers.subtraction(5, 7);
+        Numbers.multiplication(5, 7);
+        Numbers.division(5, 7);
+        Numbers.modulo(5 ,7);
+    }
+}
---
+++ src/main/java/org/fasttrackit/Numbers.java
@@ -0,0 +1,29 @@
+package org.fasttrackit;
+
+public class Numbers {
+    public static int addition(int x, int y){
+        int z = x + y;
+        System.out.println("Result of the addition is: "+z);
+        return z;
+    }
+    public static int subtraction(int x, int y){
+        int z = x - y;
+        System.out.println("Result of the subtraction is: "+z);
+        return z;
+    }
+    public static int multiplication(int x, int y){
+        int z = x * y;
+        System.out.println("Result of the multiplication is: "+z);
+        return z;
+    }
+    public static double division(double x, double y){
+        double z = x / y;
+        System.out.println("Result of the division is: "+z);
+        return z;
+    }
+    public static double modulo(int x, int y){
+        int z = x - y;
+        System.out.println("Result of the modulo is: "+z);
+        return z;
+    }
+}
