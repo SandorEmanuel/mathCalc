406906d6ad05d9d90de966b1962039cab7abed80
SandorEmanuel <sandor_emi@yahoo.com>
2019-01-14 16:30:56 +0200

    set-up methods overloading

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -9,5 +9,11 @@ public class App
         Numbers.multiplication(5, 7);
         Numbers.division(5, 7);
         Numbers.modulo(5 ,7);
+
+        Numbers.addition(5.25, 7.44);
+        Numbers.subtraction(5.25, 7.44);
+        Numbers.multiplication(5.25, 7.44);
+        Numbers.division(5.25, 7.44);
+        Numbers.modulo(5.25 ,7.44);
     }
 }
--- src/main/java/org/fasttrackit/Numbers.java
+++ src/main/java/org/fasttrackit/Numbers.java
@@ -1,29 +1,62 @@
 package org.fasttrackit;
 
 public class Numbers {
-    public static int addition(int x, int y){
+    public static int addition(int x, int y) {
         int z = x + y;
-        System.out.println("Result of the addition is: "+z);
+        System.out.println("Result of the addition is: " + z);
         return z;
     }
-    public static int subtraction(int x, int y){
+
+    public static double addition(double x, double y) {
+        double z = x + y;
+        System.out.println("Result of the addition is: " + z);
+        return z;
+    }
+
+    public static int subtraction(int x, int y) {
         int z = x - y;
-        System.out.println("Result of the subtraction is: "+z);
+        System.out.println("Result of the subtraction is: " + z);
         return z;
     }
-    public static int multiplication(int x, int y){
+
+    public static double subtraction(double x, double y) {
+        double z = x - y;
+        System.out.println("Result of the subtraction is: " + z);
+        return z;
+    }
+
+    public static int multiplication(int x, int y) {
         int z = x * y;
-        System.out.println("Result of the multiplication is: "+z);
+        System.out.println("Result of the multiplication is: " + z);
         return z;
     }
-    public static double division(double x, double y){
+    public static double multiplication(double x, double y) {
+        double z = x * y;
+        System.out.println("Result of the multiplication is: " + z);
+        return z;
+    }
+    public static int division(int x, int y) {
+        int z = x / y;
+        System.out.println("Result of the division is: " + z);
+        return z;
+    }
+
+    public static double division(double x, double y) {
         double z = x / y;
-        System.out.println("Result of the division is: "+z);
+        System.out.println("Result of the division is: " + z);
         return z;
     }
-    public static double modulo(int x, int y){
+
+    public static int modulo(int x, int y) {
         int z = x - y;
-        System.out.println("Result of the modulo is: "+z);
+        System.out.println("Result of the modulo is: " + z);
+        return z;
+    }
+    public static double modulo(double x, double y) {
+        double z = x - y;
+        System.out.println("Result of the modulo is: " +z);
         return z;
     }
 }
+
+
